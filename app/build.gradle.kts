20f597bedbb3989194b12b7af7b8de65750e107a
yvliet <recrified@gmail.com>
2026-09-19 16:08:16 +0700

    chore: purge unused assets and optimize texture density

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.phosphor.icons)
